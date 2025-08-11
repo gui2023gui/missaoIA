bb108a18ba97b06053c7b43a500a1c8ef79d608a
gui2023gui <140444905+gui2023gui@users.noreply.github.com>
2025-08-11 11:50:28 -0300

    aula 2

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-const caixaPrincipal = document.querySelector()
+const caixaPrincipal = document.querySelector(".caixa-Princial");
+const caixaPerguntas = document.querySelector(".caixa-Perguntas");
+const caixaAlternativa = document.querySelector(".caixa-Alternativa");
+const caixaResultado = document.querySelector(".caixa-Resultado");
+const TextoResultado = document.querySelector(".texto-Resultado");
+
+const perguntas = [
+    {
+        enunciado: "pergunta1",
+        alternativas: [
+            "alternativas 1",
+            "alternativas 2"]
+    }
+    {
+        enunciado: "pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"]
+    }
+]
