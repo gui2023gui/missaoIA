71194098c674a86387e27b6e12e8ff21a08e68b3
gui2023gui <140444905+gui2023gui@users.noreply.github.com>
2025-08-25 11:45:19 -0300

    perguntas

--- script.js
+++ script.js
@@ -6,15 +6,34 @@ const TextoResultado = document.querySelector(".texto-Resultado");
 
 const perguntas = [
     {
-        enunciado: "pergunta1",
+        enunciado: "Qual banda britânica, famosa por sucessos como 'Bohemian Rhapsody' e 'We Will Rock You', tinha Freddie Mercury como vocalista?",
         alternativas: [
-            "alternativas 1",
-            "alternativas 2"]
+            "Queen",
+            "The Beatles"]
     }
     {
-        enunciado: "pergunta 2",
+        enunciado: "Conhecido como o 'Rei do Rock and Roll', qual artista americano popularizou o gênero nos anos 50 com músicas como 'Jailhouse Rock'?",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"]
+            "Chuck Berry",
+            "Elvis Presley
+"]
+    }
+    {
+        enunciado: "Qual banda de Seattle, considerada uma das pioneiras do movimento grunge, lançou o icônico álbum 'Nevermind' em 1991?",
+        alternativas: [
+            "Pearl Jam",
+            "Nirvana"]
+    }
+    {
+        enunciado: "Qual banda de rock and roll australiana é conhecida pela música 'Highway to Hell'?",
+        alternativas: [
+            "Led Zeppelin",
+            "AC/DC"]
+    }
+    {
+        enunciado: "Quem é a vocalista da banda de rock 'Blondie', conhecida por hits como 'Heart of Glass'?",
+        alternativas: [
+            "Debbie Harry",
+            "Joan Jett"]
     }
 ]
